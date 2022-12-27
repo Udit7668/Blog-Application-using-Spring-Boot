c1aad55f8c42d253aa420da64c7040c4af14d7fb
Udit7668 <294uditmishra@gmail.com>
2022-12-27 14:03:03 +0530

    user repository and user service class added

---
+++ src/main/java/com/udit/blogapplication/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.udit.blogapplication.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.udit.blogapplication.entities.User;
+
+public interface UserRepository extends CrudRepository<User,Integer>{
+    
+}
---
+++ src/main/java/com/udit/blogapplication/services/UserService.java
@@ -0,0 +1,18 @@
+package com.udit.blogapplication.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udit.blogapplication.entities.User;
+import com.udit.blogapplication.repository.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    public void addUser(User user){
+        this.userRepository.save(user);
+    }
+}
