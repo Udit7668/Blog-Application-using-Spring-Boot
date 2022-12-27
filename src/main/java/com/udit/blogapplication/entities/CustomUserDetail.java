6a4bcf266dc34080982befbd434c35a1a610a089
Udit7668 <294uditmishra@gmail.com>
2022-12-27 14:41:42 +0530

    customuserdetail java class is added

---
+++ src/main/java/com/udit/blogapplication/entities/CustomUserDetail.java
@@ -0,0 +1,60 @@
+package com.udit.blogapplication.entities;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetail implements UserDetails {
+
+
+    private User user;
+
+    
+    public CustomUserDetail(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+            HashSet<SimpleGrantedAuthority> hashSet=new HashSet<>();
+            hashSet.add(new SimpleGrantedAuthority(this.user.getRole()));
+        return hashSet;
+    }
+
+    @Override
+    public String getPassword() {
+                return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        
+        return this.user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+                return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+               return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        
+        return true;
+    }
+    
+}
