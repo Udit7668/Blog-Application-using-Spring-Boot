bbeae11386a59ed8de2a79560282862cccd87d06
Udit7668 <294uditmishra@gmail.com>
2023-01-01 18:50:45 +0530

    getting all post by rest api

--- src/main/java/com/udit/blogapplication/restapi/PostControllerRestApi.java
+++ src/main/java/com/udit/blogapplication/restapi/PostControllerRestApi.java
@@ -1,24 +1,41 @@
 package com.udit.blogapplication.restapi;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udit.blogapplication.entities.Post;
 import com.udit.blogapplication.repository.PostRepository;
+import com.udit.blogapplication.services.PostService;
 
 @RestController
 @RequestMapping("/getPosts")
 public class PostControllerRestApi {
 
     @Autowired
-    private PostRepository postRepository;
+    private PostService postService;
     
     @GetMapping("/{id}")
     public Post getPostById(@PathVariable("id") Integer id){ 
-    Post post=(Post) this.postRepository.findPostById(id);     
+    Post post=this.postService.getPostById(id);     
     return post;
     }
+
+    @GetMapping("/")
+    public List<Post> getAllPost(){
+        List<Post> posts=this.postService.getAllPost();
+        return posts;
+    }
+
+    @PostMapping("/add")
+   public Post addPost(@RequestBody Post post){
+    return null;
+   }
+
 }
