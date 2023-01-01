48ae50221f28d27f035cfc78051fb236fa502717
Udit7668 <294uditmishra@gmail.com>
2023-01-01 23:35:41 +0530

    sending response with response entity

--- src/main/java/com/udit/blogapplication/restapi/PostControllerRestApi.java
+++ src/main/java/com/udit/blogapplication/restapi/PostControllerRestApi.java
@@ -1,9 +1,14 @@
 package com.udit.blogapplication.restapi;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udit.blogapplication.entities.Post;
-import com.udit.blogapplication.repository.PostRepository;
 import com.udit.blogapplication.services.PostService;
 
 @RestController
@@ -23,15 +27,21 @@ public class PostControllerRestApi {
     private PostService postService;
     
     @GetMapping("byId/{id}")
-    public Post getPostById(@PathVariable("id") Integer id){ 
-    Post post=this.postService.getPostById(id);     
-    return post;
+    public ResponseEntity<Post> getPostById(@PathVariable("id") Integer id){ 
+    Post post=this.postService.getPostById(id);    
+    if(post==null){
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    } 
+    return ResponseEntity.of(Optional.of(post));
     }
 
     @GetMapping("/")
-    public List<Post> getAllPost(){
+    public ResponseEntity<List<Post>> getAllPost(){
         List<Post> posts=this.postService.getAllPost();
-        return posts;
+        if(posts.size()<=0){
+          return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.of(posts));
     }
 
   @PostMapping("/add")
@@ -52,9 +62,35 @@ public class PostControllerRestApi {
     return posts;
    }
 
-@GetMapping("search/{searchBy}")
+@GetMapping("/search/{searchBy}")
    public List<Post> getAllPostBySearch(@PathVariable("searchBy") String searchBy){
 List<Post> posts=this.postService.getAllPostByTitle(searchBy);
     return posts;
    }
+
+
+   @GetMapping("/filter/{authors}/{tags}/{date}")
+   public List<Post> getAllPostByFilter(@PathVariable(value = "authors",required=false) String author,
+   @PathVariable(value = "tags",required=false) String tag,
+   @PathVariable(value = "date",required=false) String dates) throws ParseException{
+  
+    List<String> authors=convert(author);
+    List<String> tags=convert(tag);
+    List<String> date=convert(dates);
+    Set<Post> posts = this.postService.getAllPostByFilter(authors, tags,date);
+    List<Post> listOfPosts=new ArrayList<>(posts);
+    return listOfPosts;
+   }
+
+
+   public List<String> convert(String string){
+    String[] list=string.split(",");
+    List<String> listOf=new ArrayList<>();
+    for(String single:list){
+      listOf.add(single);
+    }
+    return listOf;
+   }
 }
+
+
