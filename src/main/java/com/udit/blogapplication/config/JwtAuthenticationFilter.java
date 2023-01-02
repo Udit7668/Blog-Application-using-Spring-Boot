384c7723e974be323dc8ff537ca81cb460501b0c
Udit7668 <294uditmishra@gmail.com>
2023-01-03 03:12:12 +0530

    all completed in blog project

--- src/main/java/com/udit/blogapplication/config/JwtAuthenticationFilter.java
+++ src/main/java/com/udit/blogapplication/config/JwtAuthenticationFilter.java
@@ -20,48 +20,48 @@ import com.udit.blogapplication.helper.JwtUtil;
 import com.udit.blogapplication.services.CustomeUserDetailService;
 
 @Component
-public class JwtAuthenticationFilter extends OncePerRequestFilter{
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
     private CustomeUserDetailService customeUserDetailService;
 
     @Autowired
     private JwtUtil jwtUtil;
-   
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        
-     String requestTokenHeader= request.getHeader("Authorization");
 
-     String username=null;
-     String jwtToken=null;
- 
-     if(requestTokenHeader!=null && requestTokenHeader.startsWith("Bearer ")){
-        jwtToken=requestTokenHeader.substring(7);
-        try{
-      username=this.jwtUtil.extractUsername(jwtToken);
+        String requestTokenHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String jwtToken = null;
+
+        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
+            jwtToken = requestTokenHeader.substring(7);
+            try {
+                username = this.jwtUtil.extractUsername(jwtToken);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            UserDetails userDetails = this.customeUserDetailService.loadUserByUsername(username);
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+
+                usernamePasswordAuthenticationToken
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            } else {
+                System.out.println("Token is not validated");
+            }
+
         }
-        catch(Exception e){
-      e.printStackTrace();
-        }
 
-     UserDetails userDetails= this.customeUserDetailService.loadUserByUsername(username);
-        if(username!=null && SecurityContextHolder.getContext().getAuthentication()==null){
-  UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken= new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
-  
-  usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        filterChain.doFilter(request, response);
 
-  SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
     }
-        else{
-            System.out.println("Token is not validated");
-        }
 
-     }
-
-     filterChain.doFilter(request, response);
-        
-    }
-    
 }
