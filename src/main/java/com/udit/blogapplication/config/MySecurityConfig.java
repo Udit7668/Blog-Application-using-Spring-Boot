7de5b2d02dc7a866c7c3a1ad3938f05ae8cfd9ec
Udit7668 <294uditmishra@gmail.com>
2023-01-03 02:33:18 +0530

    successfuly generated the token

--- src/main/java/com/udit/blogapplication/config/MySecurityConfig.java
+++ src/main/java/com/udit/blogapplication/config/MySecurityConfig.java
@@ -42,8 +42,6 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter{
         .exceptionHandling()
         .accessDeniedPage("/access-denied")
         ;
-
-    
     }
 
     @Override
